64d6bce2d9aff842d5981af61409c2784137d57e
Henry Ripple <hripple46@gmail.com>
2023-03-03 15:32:08 -0600

    added 'highscore' counter that will always show the highest score without resetting

--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 export default function Gameboard(props) {
+  let [highScore, setHighScore] = useState(0);
+
+  let updateHighScore = () => {
+    if (props.currentScore > highScore) {
+      setHighScore(props.currentScore);
+    }
+  };
+
   return (
     <div>
+      {updateHighScore(props.currentScore)}
       <div id="currentScore">
         <h2>Current Score: </h2>
         <h3>{props.currentScore}</h3>
       </div>
       <div id="highScore">
         <h2>High Score: </h2>
-        <h3>{props.highScore}</h3>
+        <h3>{highScore}</h3>
       </div>
     </div>
   );
